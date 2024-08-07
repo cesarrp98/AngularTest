02706c17984a1a508a82d5d3c1dfda3ca66f3416
Cesar Rodriguez <cesarrodriguezpiloto@gmail.com>
2024-08-07 18:25:57 -0400

    Creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MainComponent } from './main/main.component';
+import { FilterComponent } from './filter/filter.component';
+import { ListComponent } from './list/list.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FilterComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule
   ],
   providers: [
     provideAnimationsAsync()
